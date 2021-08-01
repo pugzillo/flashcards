950980c043bc9bdaefefc0ac184f31091f2e0afb
Linh <lcahu91@gmail.com>
2021-08-01 17:22:17 -0400

    added routing for home and create deck

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import DeckList from "./Home/DeckList";
+import CreateDeck from  "./CreateDeck";
+import { Route, Switch } from "react-router-dom";
 
 function Layout() {
   return (
-    <>
+    <Fragment>
       <Header />
       <div className="container">
         {/* TODO: Implement the screen starting here */}
-        <NotFound />
+        <Switch>
+          <Route exact path="/">
+            <DeckList />
+          </Route>
+
+          <Route exact path="/decks/new">
+            <CreateDeck />
+          </Route>
+
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
-    </>
+    </Fragment>
   );
 }
 
