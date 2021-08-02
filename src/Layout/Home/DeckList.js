d056f461f21580c34222abb7b37129b36f14729d
Linh <lcahu91@gmail.com>
2021-08-02 15:05:01 -0400

    moved routes to layout

--- src/Layout/Home/DeckList.js
+++ src/Layout/Home/DeckList.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Switch, Route } from "react-router-dom";
 import Deck from "./Deck";
+import CreateDeck from "../CreateDeck";
+import DeckHome from "../DeckHome/DeckHome";
 import { listDecks } from "../../utils/api";
 
 function DeckList() {
   const [decks, setDecks] = useState([]);
-  
+
   useEffect(() => {
     const abortController = new AbortController();
     listDecks(abortController.signal).then(setDecks);
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,7 +2,10 @@ import React, { Fragment } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
 import DeckList from "./Home/DeckList";
-import CreateDeck from  "./CreateDeck";
+import CreateDeck from "./CreateDeck";
+import DeckHome from "./DeckHome/DeckHome";
+import DeckEdit from "./DeckHome/DeckEdit";
+import DeckStudy from "./DeckHome/DeckStudy";
 import { Route, Switch } from "react-router-dom";
 
 function Layout() {
@@ -12,14 +15,21 @@ function Layout() {
       <div className="container">
         {/* TODO: Implement the screen starting here */}
         <Switch>
-          <Route exact path="/">
-            <DeckList />
-          </Route>
-
-          <Route exact path="/decks/new">
+          <Route path="/decks/new">
             <CreateDeck />
           </Route>
-
+          <Route path="/decks/:deckId/study">
+            <DeckStudy />
+          </Route>
+          <Route path="/decks/:deckId/edit">
+            <DeckEdit />
+          </Route>
+          <Route path="/decks/:deckId">
+            <DeckHome />
+          </Route>
+          <Route path="/">
+            <DeckList />
+          </Route>
           <Route>
             <NotFound />
           </Route>
