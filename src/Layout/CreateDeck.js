f73c95834aee51892fab4875efffac0dcdc265be
Linh <lcahu91@gmail.com>
2021-08-02 10:23:55 -0400

    added link to cancel button

--- src/Layout/CreateDeck.js
+++ src/Layout/CreateDeck.js
@@ -3,28 +3,12 @@ import { Link } from "react-router-dom";
 
 function CreateDeck() {
   return (
-    <div>
+    <div className="container">
       <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
           <li class="breadcrumb-item">
             <Link to="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="currentColor"
-                class="bi bi-house"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
-                />
-                <path
-                  fill-rule="evenodd"
-                  d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
-                />
-              </svg>
+              <span class="oi oi-home"></span>
               Home
             </Link>
           </li>
@@ -34,7 +18,41 @@ function CreateDeck() {
         </ol>
       </nav>
 
-</div>
+      <h1>Create Deck</h1>
+
+      <form>
+        <div class="mb-3">
+          <label for="name" class="form-label">
+            Name
+          </label>
+          <input
+            type="name"
+            class="form-control"
+            id="name"
+            aria-describedby="deckName"
+            placeholder="Deck Name"
+          ></input>
+        </div>
+        <div class="mb-3">
+          <label for="description" class="form-label">
+            Deck Description
+          </label>
+          <textarea
+            type="email"
+            class="form-control"
+            id="description"
+            aria-describedby="deckName"
+            placeholder="Deck Description"
+          ></textarea>
+        </div>
+        <Link to="/" class="btn btn-secondary" href="#" role="button">
+          Cancel
+        </Link>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 
