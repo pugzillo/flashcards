0a9f6c45a814062415ba3e8e6223ea1b750ef940
Linh <lcahu91@gmail.com>
2021-08-03 18:46:10 -0400

    removed old code

--- src/Layout/CreateDeck.js
+++ src/Layout/CreateDeck.js
@@ -15,16 +15,14 @@ function CreateDeck() {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    createDeck(deck); 
+    createDeck(deck);
     window.alert(`You created a new deck: ${deck.name}`);
     history.push("/");
   };
 
-  const breadCrumbLinks = [
-    { dir: "/decks/new", label: "Create Deck" },
-  ];
+  const breadCrumbLinks = [{ dir: "/decks/new", label: "Create Deck" }];
 
-  const cancelLink = "/"; 
+  const cancelLink = "/";
 
   return (
     <div className="container">
@@ -32,46 +30,13 @@ function CreateDeck() {
 
       <h1>Create Deck</h1>
 
-      <Form cancelLink={cancelLink} deckName={deck.name} deckDescription={deck.description} submitHandler={submitHandler} changeHandler={changeHandler} />
-
-      {/* <form onSubmit={submitHandler}>
-        <div className="mb-3">
-          <label for="name" class="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="name"
-            name="name"
-            aria-describedby="deckName"
-            placeholder="Deck Name"
-            onChange={changeHandler}
-            value={deck.name}
-          ></input>
-        </div>
-        <div class="mb-3">
-          <label for="description" class="form-label">
-            Deck Description
-          </label>
-          <textarea
-            type="text"
-            class="form-control"
-            id="description"
-            name="description"
-            aria-describedby="deckDescription"
-            placeholder="Deck Description"
-            onChange={changeHandler}
-            value={deck.description}
-          ></textarea>
-        </div>
-        <Link to="/" class="btn btn-secondary" href="#" role="button">
-          Cancel
-        </Link>
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form> */}
+      <Form
+        cancelLink={cancelLink}
+        deckName={deck.name}
+        deckDescription={deck.description}
+        submitHandler={submitHandler}
+        changeHandler={changeHandler}
+      />
     </div>
   );
 }
