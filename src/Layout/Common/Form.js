82a40c8b0ba70b3e9c34e0c91e7390a84c36cab0
Linh <lcahu91@gmail.com>
2021-08-03 18:36:14 -0400

    changed file name; broke form into individual component; added form to deck edit page

---
+++ src/Layout/Common/Form.js
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+
+function Form({ deckId, deckName, deckDescription, submitHandler, changeHandler}) {
+  return (
+    <form onSubmit={ submitHandler }>
+      <div className="mb-3">
+        <label for="name" class="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          class="form-control"
+          id="name"
+          name="name"
+          aria-describedby="deckName"
+          placeholder="Deck Name"
+          onChange={changeHandler}
+          value={deckName}
+        ></input>
+      </div>
+      <div class="mb-3">
+        <label for="description" class="form-label">
+          Deck Description
+        </label>
+        <textarea
+          type="text"
+          class="form-control"
+          id="description"
+          name="description"
+          aria-describedby="deckDescription"
+          placeholder="Deck Description"
+          onChange={changeHandler}
+          value={deckDescription}
+        ></textarea>
+      </div>
+      <Link to={`/decks/${deckId}`} class="btn btn-secondary" href="#" role="button">
+        Cancel
+      </Link>
+      <button type="submit" class="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default Form;
--- src/Layout/DeckHome/EditDeck.js
+++ src/Layout/DeckHome/EditDeck.js
@@ -1,11 +1,52 @@
+import { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router";
+import { readDeck, updateDeck } from "../../utils/api";
 import Breadcrumb from "../Common/Breadcrumb";
+import Form from "../Common/Form";
 
-function DeckEdit() {
+function EditDeck() {
+  const { deckId } = useParams();
+  const [deck, setDeck] = useState({}); // used for breadcrumb; have both so deck var can be changed separately
+  const [newDeck, setNewDeck] = useState({}); // used for form
+
+  // Old Deck info
+  useEffect(() => {
+    const abortController = new AbortController();
+    readDeck(deckId, abortController.signal).then(setDeck);
+    return () => abortController.abort();
+  }, [deckId]);
+
+  // New Deck Info
+  useEffect(() => {
+    const abortController = new AbortController();
+    readDeck(deckId, abortController.signal).then(setNewDeck);
+    return () => abortController.abort();
+  }, [deckId]);
+
+  const changeHandler = (event) => {
+    setNewDeck({...newDeck, [event.target.name]: event.target.value });
+  };
+
+  const history = useHistory();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    updateDeck(newDeck);
+    history.go(0); // Refresh after submit
+  };
+
   const breadCrumbLinks = [
-    { dir: "/decks/new", label: "deck" },
-    { dir: "/", label: "home" }
+    { dir: "/decks/new", label: `${deck.name}` },
+    { dir: "/", label: "Edit Card" },
   ];
-  return <Breadcrumb links={breadCrumbLinks} />;
+  return (
+    <div className="container">
+      <Breadcrumb links={breadCrumbLinks} />
+
+      <h1>Edit Deck</h1>
+      <Form deckId={deckId} deckName={newDeck.name} deckDescription={newDeck.description} submitHandler={submitHandler} changeHandler={changeHandler} />
+    </div>
+  );
 }
 
-export default DeckEdit;
+export default EditDeck;
