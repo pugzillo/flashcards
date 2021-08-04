f7d9b9fe58a21f292cff5f7f51fd92ffe73b805c
Linh <lcahu91@gmail.com>
2021-08-04 11:39:41 -0400

    added breadcrumb

--- src/Layout/Cards/AddCard.js
+++ src/Layout/Cards/AddCard.js
@@ -1,5 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import CardForm from "../Common/CardForm";
+import Breadcrumb from "../Common/Breadcrumb";
+import { readDeck } from "../../utils/api";
+
 function AddCard() {
-    return <h1>Add Card</h1>
+  const { deckId } = useParams();
+  const [deck, setDeck] = useState();
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    readDeck(deckId, abortController.signal).then(setDeck);
+    return () => abortController.abort();
+  }, [deckId]);
+
+  console.log(deck)
+
+  const breadCrumbLinks = [
+    { dir: "/decks/new", label: `${deck.name}` },
+    { dir: "", label: "Add Card" },
+  ];
+
+  return (
+    <div className="container">
+      <Breadcrumb links={breadCrumbLinks} />
+    </div>
+  );
 }
 
-export default AddCard; 
+export default AddCard;
