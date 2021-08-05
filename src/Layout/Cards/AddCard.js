e3e3d0b22623b025498ba194870e58c0acd3af19
Linh <lcahu91@gmail.com>
2021-08-05 14:10:08 -0400

    added comments

--- src/Layout/Cards/AddCard.js
+++ src/Layout/Cards/AddCard.js
@@ -5,27 +5,30 @@ import Breadcrumb from "../Common/Breadcrumb";
 import { createCard, readDeck } from "../../utils/api";
 
 function AddCard() {
+  /* Add new card to a deck */
   const { deckId } = useParams();
   const [deck, setDeck] = useState({});
   const [card, setCard] = useState({});
 
+  // Current Deck information
   useEffect(() => {
     const abortController = new AbortController();
     readDeck(deckId, abortController.signal).then(setDeck);
     return () => abortController.abort();
   }, [deckId]);
 
+  // Add input to the card object
   const changeHandler = (event) => {
     setCard({ ...card, [event.target.name]: event.target.value });
   };
 
-  const history = useHistory(); 
+  const history = useHistory();
 
   const submitHandler = (event) => {
     event.preventDefault();
     createCard(deckId, card);
     window.alert(`You created a new card`);
-    history.go(0); 
+    history.go(0); // refresh the page when user creates a new card
   };
 
   const breadCrumbLinks = [
