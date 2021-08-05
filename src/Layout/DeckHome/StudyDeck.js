212efcb855881ae4b8c637e7e8a1920ca8bd5a20
Linh <lcahu91@gmail.com>
2021-08-05 13:58:13 -0400

    replaced hardcoded bredcrumb w/componont

--- src/Layout/DeckHome/StudyDeck.js
+++ src/Layout/DeckHome/StudyDeck.js
@@ -2,15 +2,16 @@ import React, { useState, useEffect } from "react";
 import { Link, useParams, useHistory } from "react-router-dom";
 import { readDeck } from "../../utils/api";
 import FlipCard from "../Cards/FlipCard";
+import Breadcrumb from "../Common/Breadcrumb";
 
 function DeckStudy() {
   const { deckId } = useParams();
   const [deck, setDeck] = useState({});
-  let cards = []; 
+  let cards = [];
 
   if (deck.cards) {
-    cards = deck.cards; 
-  };
+    cards = deck.cards;
+  }
 
   const [currentCard, setCurrentCard] = useState(0);
   const [orientation, setOrientation] = useState("front");
@@ -29,61 +30,49 @@ function DeckStudy() {
     } else if (orientation === "back") {
       setOrientation("front");
     }
-  }; 
+  };
 
   const HandleNext = () => {
-    if (currentCard + 1 === cards.length && window.confirm("Restart cards? Click 'cancel' to return to the homepage")){
-      history.push('/');
+    if (
+      currentCard + 1 === cards.length &&
+      window.confirm("Restart cards? Click 'cancel' to return to the homepage")
+    ) {
+      history.push("/"); // sends user to home page
     }
-    setCurrentCard(currentCard+1); 
-  }
+    setCurrentCard(currentCard + 1); // moves use to next card
+  };
+
+  const breadCrumbLinks = [
+    { dir: `/decks/${deckId}`, label: `${deck.name}` },
+    { dir: `/decks/${deckId}/study`, label: "Study" },
+  ];
 
   if (cards.length <= 2) {
-    return ( 
+    return (
       <div className="container">
-      <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <li className="breadcrumb-item">
-            <Link to="/">
-              <span className="oi oi-home"></span>
-              Home
-            </Link>
-          </li>
-          <li className="breadcrumb-item active" aria-current="page">
-            {deck.name}
-          </li>
-          <li className="breadcrumb-item active" aria-current="page">
-            Study
-          </li>
-        </ol>
-      </nav>
-      <h1>{`Study: ${deck.name}`}</h1>
-      <h2>Not enough cards.</h2>
-      <p>{`You need at least 3 cards to study. There are ${cards.length} in this deck.`}</p>
-      <p><Link to={`/decks/${deckId}/cards/new`}>Add Card</Link></p>
-    </div>
-    )
+        <Breadcrumb links={breadCrumbLinks} />
+        <h1>{`Study: ${deck.name}`}</h1>
+        <h2>Not enough cards.</h2>
+        <p>{`You need at least 3 cards to study. There are ${cards.length} in this deck.`}</p>
+        <p>
+          <Link to={`/decks/${deckId}/cards/new`}>Add Card</Link>
+        </p>
+      </div>
+    );
   } else if (deck && cards[currentCard]) {
     return (
       <div className="container">
-        <nav aria-label="breadcrumb">
-          <ol className="breadcrumb">
-            <li className="breadcrumb-item">
-              <Link to="/">
-                <span className="oi oi-home"></span>
-                Home
-              </Link>
-            </li>
-            <li className="breadcrumb-item active" aria-current="page">
-              {deck.name}
-            </li>
-            <li className="breadcrumb-item active" aria-current="page">
-              Study
-            </li>
-          </ol>
-        </nav>
+        <Breadcrumb links={breadCrumbLinks} />
         <h1>{`Study: ${deck.name}`}</h1>
-        <FlipCard currentCard={currentCard+1} length={cards.length} front={cards[currentCard].front} back={cards[currentCard].back} orientation={orientation} HandleFlip={HandleFlip} HandleNext={HandleNext} />
+        <FlipCard
+          currentCard={currentCard + 1}
+          length={cards.length}
+          front={cards[currentCard].front}
+          back={cards[currentCard].back}
+          orientation={orientation}
+          HandleFlip={HandleFlip}
+          HandleNext={HandleNext}
+        />
       </div>
     );
   } else {
