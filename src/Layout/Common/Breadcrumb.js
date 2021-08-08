96ce8db377156dcd93d8f2c04ef7130d694b711c
Linh <lcahu91@gmail.com>
2021-08-08 16:45:20 -0400

    added keys to li elements

--- src/Layout/Common/Breadcrumb.js
+++ src/Layout/Common/Breadcrumb.js
@@ -6,15 +6,15 @@ function Breadcrumb({ links }) {
   Takes in array of objects that is used to create links in Breadcrumb nav menu; Already includes Home.
   */
   const linkList = links.map((link, index) => (
-    <li className="breadcrumb-item active" aria-current="page">
-      <Link to={links.dir} key={index+1}>{link.label}</Link>
+    <li className="breadcrumb-item active" aria-current="page" key={(index+1).toString()}>
+      <Link to={links.dir}>{link.label}</Link>
     </li>
   ));
   return (
     <div className="container">
       <nav aria-label="breadcrumb">
         <ol className="breadcrumb">
-          <li className="breadcrumb-item" key={0}>
+          <li className="breadcrumb-item" key="0">
             <Link to="/">
               <span className="oi oi-home"></span>
               Home
